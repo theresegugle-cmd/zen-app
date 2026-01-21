822c7f45a57f0d102183225ef714d5a044fe367f
vibe as <theresegugle@gmail.com>
2026-01-21 22:55:18 +0100

    Add files via upload

---
+++ node.js
@@ -0,0 +1,22 @@
+﻿import React from "react";
+import { View, Text } from "react-native";
+
+
+export default function CalmScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Pust inn ro. Pust ut spenning.</Text>
+    </View>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+npm install -g expo-cli
